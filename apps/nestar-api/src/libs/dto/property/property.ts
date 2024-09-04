ca6c5fe0a099ae95cc4be90b9a0642126e82d39f
Akhrorfayzullo <akhrorfayzullo@gmail.com>
2024-09-04 14:26:12 +0900

    feat: removePropertyByAdmin

--- apps/nestar-api/src/libs/dto/property/property.ts
+++ apps/nestar-api/src/libs/dto/property/property.ts
@@ -1,6 +1,7 @@
 import { Field, Int, ObjectType } from '@nestjs/graphql';
 import { ObjectId } from 'mongoose';
 import { PropertyLocation, PropertyStatus, PropertyType } from '../../enums/property.enum';
+import { Member, TotalCounter } from '../member/member';
 
 @ObjectType()
 export class Property {
@@ -75,4 +76,16 @@ export class Property {
 
   @Field(() => Date)
   updatedAt: Date;
+
+  @Field(() => Member, {nullable: true})
+  memberData?: Member;
+}
+
+@ObjectType()
+export class Properties{
+  @Field(() => [Property])
+  list: Property[];
+
+  @Field(() => [TotalCounter], {nullable: true})
+  metaCounter: TotalCounter[];
 }
